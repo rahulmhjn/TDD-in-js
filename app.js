c1e5642d5fd04adb557ceb5860386cb50ac57cef
rahulmhjn <rahul.mahajan676@gmail.com>
2022-03-23 12:21:10 +0530

    correcting the syntax of negative testing in testing file and adding a condition for negative check in main file

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ class StringCalculator {
       .split(",") // Split values
       .map((n) => parseInt(n, 10)); // Convert to integers
 
+    if (numbers.some((n) => n < 0)) throw new Error("negatives not allowed");
+
     return numbers.reduce((s, n) => s + n);
   }
 }
