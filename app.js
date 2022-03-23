87d90da90b38d732f131e6657d572445ade6f130
rahulmhjn <rahul.mahajan676@gmail.com>
2022-03-23 12:16:09 +0530

    building the logic to add numbers by following the failed test cases

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ class StringCalculator {
     if (value === "") {
       return 0;
     }
+    const numbers = value
+      .replace(/(\r\n|\n|\r)/gm, ",") // Normalize delimiter
+      .split(",") // Split values
+      .map((n) => parseInt(n, 10)); // Convert to integers
+
+    return numbers.reduce((s, n) => s + n);
   }
 }
 
--- app.test.js
+++ app.test.js
@@ -15,27 +15,27 @@ describe("StringCalculator", () => {
 
   //Test 2 - for a single number in string it should return the same number
   it("for a single number in string it should return the same number", () => {
-    expect(calculator.add("1")).toBe(1);
+    expect(stringCalc.add("1")).toBe(1);
   });
 
   //Test 3 - for two numbers it should return the sum
   it("for two numbers it should return the sum", () => {
-    expect(calculator.add("1,2")).toBe(3);
+    expect(stringCalc.add("1,2")).toBe(3);
   });
 
   //Test 4 - should return the sum of an unknown amount of numbers
   it("should return the sum of unknown amount of numbers", () => {
-    expect(calc.add("133,36,1,3,5")).toBe(178);
-    expect(calc.add("1,2,90,100")).toBe(193);
+    expect(stringCalc.add("133,36,1,3,5")).toBe(178);
+    expect(stringCalc.add("1,2,90,100")).toBe(193);
   });
 
   //Test 5 - allow \n delimiter between numbers
   it("allow \n delimiter between numbers", function () {
-    expect(calculator.add("1\n2,3")).toBe(6);
+    expect(stringCalc.add("1\n2,3")).toBe(6);
   });
 
   //Test 6 - does not allow negative numbers
   it("does not like negative numbers", () => {
-    expect(calc.add("-41,50")).toThrow("negatives not allowed -41");
+    expect(() => stringCalc.add("-41,50").toThrow("negatives not allowed"));
   });
 });
